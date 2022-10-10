e002e464449f716fe22aa15a7b63e4fa7e64bd20
Yoga Anggara <doctorbean31@gmail.com>
2022-10-10 23:13:00 +0700

    Backend Lelang
    Koneksi ke DB xampp dan membuat DB lelang

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,28 @@
 import express from "express";
 import cors from "cors";
 import session from "express-session";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        //setting secure nya automatis jika pake http atau https
+        secure: 'auto'
+    }
+}));
+
+app.use(cors({
+    //untuk frontend mengirimkan request beserta cookie dan dengan credentials
+    credentials: true,
+    //domain yg di ijinkan untuk akses API
+    origin: 'http://localhost:3000'
+}));
+app.use(express.json());
 
 app.listen(process.env.APP_PORT, () => {
     console.log('Server Lelang up and runnng!!');
