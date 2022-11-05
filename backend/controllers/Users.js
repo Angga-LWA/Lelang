92b8b137802ad87449d1a6757d38f85d5f585b3e
Yoga Anggara <doctorbean31@gmail.com>
2022-11-05 07:04:23 +0700

    update user
    create CRUD users

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -29,12 +29,68 @@ export const getUserById = async(req, res) => {
 
 export const Register = (req, res) => {
     
+            nik: nik,
+            npwp: npwp,
+            created_by: created_by
+        });
+        res.status(201).json({msg: "Berhasil Terdaftar Anggota Lelang"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 }
 
-export const updateUser = (req, res) => {
-    
+export const updateUser = async(req, res) => {
+    const user = await User.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    const {username, first_name, last_name, email, password, confPassword, flag_active, nik, npwp, created_by} = req.body;
+    let hashPassword;
+    if(password === "" || password === null){
+        hashPassword = user.password
+    }else{
+        hashPassword = await argon2.hash(password);
+    }
+    if(password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak sesuai"});
+    try {
+        await User.update({
+            username: username,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            password, password,
+            flag_active: flag_active,
+            nik: nik,
+            npwp: npwp,
+            created_by: created_by
+        },{
+            where:{
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "Data Berhasil di Rubah"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 }
 
-export const deleteUser = (req, res) => {
-    
+export const deleteUser = async(req, res) => {
+    const user = await User.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    try {
+        await User.destroy({
+            where:{
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "Sukses Terhapus"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 }
