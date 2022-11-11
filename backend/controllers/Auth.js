972b0c24cff84dbcccefd82abdebce127eded9ba
Yoga Anggara <doctorbean31@gmail.com>
2022-11-11 19:53:54 +0700

    update auth.js
    bikin akses token

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,5 +1,6 @@
 import User from "../models/UserModel.js";
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 
 export const Login = async (req, res) =>{
     const user = await User.findOne({
@@ -7,14 +8,30 @@ export const Login = async (req, res) =>{
             email: req.body.email
         }
     });
-    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan / Password Salah"});
     const match = await argon2.verify(user.password, req.body.password);
-    if(!match) return res.status(400).json({msg: "Wrong Password"});
+    if(!match) return res.status(404).json({msg: "User tidak ditemukan / Password Salah"});
     req.session.userId = user.uuid;
     const uuid = user.uuid;
     const username = user.username;
     const email = user.email;
-    res.status(200).json({uuid, username, email});
+    const accessToken = jwt.sign({uuid, username, email}, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: '20s'
+    });
+    const refreshToken = jwt.sign({uuid, username, email}, process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn: '1d'
+    });
+    await User.update({refresh_token: refreshToken},{
+        where:{
+            uuid: uuid
+        }
+    });
+    res.cookie('refreshToken', refreshToken,{
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000
+    });
+    res.json({ accessToken });
+    // res.status(200).json({uuid, username, email, accessToken});
 }
 
 export const logOut = (req, res) =>{
