afa2005838368ad4ea0429ecedb933811b395f97
Yoga Anggara <doctorbean31@gmail.com>
2022-11-17 23:36:36 +0700

    update auth and user js
    update auth and user js

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -1,47 +1,16 @@
 import Product from "../models/ProductModel.js";
 import User from "../models/UserModel.js";
 
-export const getProducts = async(req, res) => {
-   try {
-        let response;
-        if(req.role === "admin"){
-            const response = await Product.findAll({
-                include:[{
-                    model: User
-                }]
-            });
-        }
-        res.status(200).json(response);
-   } catch (error) {
-        res.status(500).json({msg: error.message});
-   }
+export const getProducts = (req, res) => {
+   
 }
 
 export const getProductById = (req, res) => {
    
 }
 
-export const createProduct = async(req, res) => {
-    const {prdcode, prdname, prddesc, price, min_multiples, location, auction_date, created_by} = req.body;
-    try {
-        await Product.create({
-            id_region: req.id_region,
-            id_entity: req.id_entity,
-            id_branch: req.id_branch,
-            prdcode: prdcode,
-            prdname: prdname,
-            prddesc: prddesc,
-            id_category: req.id_category,
-            price: price,
-            min_multiples: min_multiples,
-            location: location,
-            auction_date: auction_date,
-            created_by: created_by
-        });
-        res.status(201).json({msg: "Sukses daftar product"});
-    } catch (error) {
-        res.status(400).json({msg: error.message});
-    }
+export const createProduct = (req, res) => {
+    
 }
 
 export const updateProduct = (req, res) => {
