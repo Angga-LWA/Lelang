044975a6680be2f88d85265e4f7fbd6cd3ac925a
Yoga Anggara <doctorbean31@gmail.com>
2022-11-30 10:19:12 +0700

    create get all product image
    create get all product image

--- backend/controllers/ProductImg.js
+++ backend/controllers/ProductImg.js
@@ -1,8 +1,29 @@
 import ProductImg from "../models/ProductImgModel.js";
-import argon2 from "argon2";
+import Product from "../models/ProductModel.js";
 
-export const getProductsImg = (req, res) => {
-   
+export const getProductsImg = async (req, res) => {
+    try {
+        let response;
+        if(req.role === "admin"){
+            response = await ProductImg.findAll({
+                include:[{
+                    model: Product
+                }]
+            });
+        }else{
+            response = await ProductImg.findAll({
+                where:{
+                    id_product: req.id_product
+                },
+                include:[{
+                    model: Product
+                }]
+            });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
 export const getProductImgById = (req, res) => {
--- backend/routes/ProductImgRoute.js
+++ backend/routes/ProductImgRoute.js
@@ -6,13 +6,14 @@ import {
     updateProductImg,
     deleteProductImg
 } from "../controllers/ProductImg.js";
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/mproduct_img', getProductsImg);
-router.get('/mproduct_img/:id', getProductImgById);
-router.post('/mproduct_img', createProductImg);
-router.patch('/mproduct_img/:id', updateProductImg);
-router.delete('/mproduct_img/:id', deleteProductImg);
+router.get('/mproduct_img',verifyUser, getProductsImg);
+router.get('/mproduct_img/:id',verifyUser, getProductImgById);
+router.post('/mproduct_img',verifyUser, createProductImg);
+router.patch('/mproduct_img/:id',verifyUser, updateProductImg);
+router.delete('/mproduct_img/:id',verifyUser, deleteProductImg);
 
 export default router;
