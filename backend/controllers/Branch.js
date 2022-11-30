c30471d70087864f6cebc2b647747b88b2d40438
Yoga Anggara <doctorbean31@gmail.com>
2022-11-30 16:31:45 +0700

    create get all Branch
    create get all Branch

--- backend/controllers/Branch.js
+++ backend/controllers/Branch.js
@@ -1,8 +1,31 @@
 import Branch from "../models/BranchModel.js";
-import argon2 from "argon2";
+import Entity from "../models/EntityModel.js";
+import Region from "../models/RegionModel.js";
 
-export const getBranch = (req, res) => {
-   
+export const getBranch = async (req, res) => {
+   try {
+        let response;
+        if(req.role === "admin"){
+            response = await Branch.findAll({
+                include:[{
+                    model: Entity, Region
+                }]
+            });
+        }else{
+            response = await Branch.findAll({
+                where:{
+                    id_entity: req.id_entity,
+                    id_region: req.id_region
+                },
+                include:[{
+                    model: Entity, Region
+                }]
+            });
+        }
+        res.status(200).json(response);
+   } catch (error) {
+        res.status(500).json({msg: error.message});
+   }
 }
 
 export const getBranchById = (req, res) => {
--- backend/routes/BranchRoute.js
+++ backend/routes/BranchRoute.js
@@ -6,13 +6,14 @@ import {
     updateBranch,
     deleteBranch
 } from "../controllers/Branch.js";
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/mbranch', getBranch);
-router.get('/mbranch/:id', getBranchById);
-router.post('/mbranch', createBranch);
-router.patch('/mbranch/:id', updateBranch);
-router.delete('/mbranch/:id', deleteBranch);
+router.get('/mbranch',verifyUser, getBranch);
+router.get('/mbranch/:id',verifyUser, getBranchById);
+router.post('/mbranch',verifyUser, createBranch);
+router.patch('/mbranch/:id',verifyUser, updateBranch);
+router.delete('/mbranch/:id',verifyUser, deleteBranch);
 
 export default router;
