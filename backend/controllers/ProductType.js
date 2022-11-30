fe0ec71d9492906a9651e7ebec1417ea5f7b50d5
Yoga Anggara <doctorbean31@gmail.com>
2022-11-30 10:06:26 +0700

    create product type get all
    create product type get all

--- backend/controllers/ProductType.js
+++ backend/controllers/ProductType.js
@@ -1,8 +1,29 @@
 import ProductType from "../models/ProductTypeModel.js";
-import argon2 from "argon2";
+import AuctionType from "../models/AuctionTypeModel.js";
 
-export const getProductsType = (req, res) => {
-   
+export const getProductsType = async (req, res) => {
+   try {
+        let response;
+        if(req.role === "admin"){
+            response = await ProductType.findAll({
+                include:[{
+                    model: AuctionType
+                }]
+            });
+        }else{
+            response = await ProductType.findAll({
+                where:{
+                    id_auctiontype: req.id_auctiontype
+                },
+                include:[{
+                    model: AuctionType
+                }]
+            });
+        }
+        res.status(200).json(response);
+   } catch (error) {
+        res.status(500).json({msg: error.message});
+   }
 }
 
 export const getProductTypeById = (req, res) => {
--- backend/routes/ProductTypeRoute.js
+++ backend/routes/ProductTypeRoute.js
@@ -6,13 +6,14 @@ import {
     updateProductType,
     deleteProductType
 } from "../controllers/ProductType.js";
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/mproduct_type', getProductsType);
-router.get('/mproduct_type/:id', getProductTypeById);
-router.post('/mproduct_type', createProductType);
-router.patch('/mproduct_type/:id', updateProductType);
-router.delete('/mproduct_type/:id', deleteProductType);
+router.get('/mproduct_type',verifyUser, getProductsType);
+router.get('/mproduct_type/:id',verifyUser, getProductTypeById);
+router.post('/mproduct_type',verifyUser, createProductType);
+router.patch('/mproduct_type/:id',verifyUser, updateProductType);
+router.delete('/mproduct_type/:id',verifyUser, deleteProductType);
 
 export default router;
