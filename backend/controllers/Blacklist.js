1b772efe088c062d2f3e1394caa7d6e1f18236c2
Yoga Anggara <doctorbean31@gmail.com>
2022-11-30 16:40:51 +0700

    create blacklist get all
    create blacklist get all

--- backend/controllers/Blacklist.js
+++ backend/controllers/Blacklist.js
@@ -1,8 +1,29 @@
 import Blacklist from "../models/BlacklistModel.js";
-import argon2 from "argon2";
+import User from "../models/UserModel.js";
 
-export const getBlacklist = (req, res) => {
-   
+export const getBlacklist = async (req, res) => {
+   try {
+        let response;
+        if(req.role === "admin"){
+            response = await Blacklist.findAll({
+                include:[{
+                    model: User
+                }]
+            });
+        }else{
+            response = await Blacklist.findAll({
+                where:{
+                    id_user: req.id_user
+                },
+                include:[{
+                    model: User
+                }]
+            });
+        }
+        res.status(200).json(response);
+   } catch (error) {
+        res.status(500).json({msg: error.message});
+   }
 }
 
 export const getBlacklistById = (req, res) => {
--- backend/routes/BlacklistRoute.js
+++ backend/routes/BlacklistRoute.js
@@ -6,13 +6,14 @@ import {
     updateBlacklist,
     deleteBlaclist
 } from "../controllers/Blacklist.js";
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/mblack_list', getBlacklist);
-router.get('/mblack_list/:id', getBlacklistById);
-router.post('/mblack_list', createBlacklist);
-router.patch('/mblack_list/:id', updateBlacklist);
-router.delete('/mblack_list/:id', deleteBlaclist);
+router.get('/mblack_list',verifyUser, getBlacklist);
+router.get('/mblack_list/:id',verifyUser, getBlacklistById);
+router.post('/mblack_list',verifyUser, createBlacklist);
+router.patch('/mblack_list/:id',verifyUser, updateBlacklist);
+router.delete('/mblack_list/:id',verifyUser, deleteBlaclist);
 
 export default router;
