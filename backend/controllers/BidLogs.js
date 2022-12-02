3fe721acc97b11b7eacb134fb92512e584a63e27
Yoga Anggara <doctorbean31@gmail.com>
2022-12-02 16:47:14 +0700

    create get all
    get all BidLog

--- backend/controllers/BidLogs.js
+++ backend/controllers/BidLogs.js
@@ -1,8 +1,29 @@
 import BidLogs from "../models/BidLogsModel.js";
-import argon2 from "argon2";
+import User from "../models/UserModel.js";
 
-export const getBidLogs = (req, res) => {
-   
+export const getBidLogs = async (req, res) => {
+   try {
+        let response;
+        if(req.role === "admin"){
+            response = await BidLogs.findAll({
+                include:[{
+                    model: User
+                }]
+            });
+        }else{
+            response = await BidLogs.findAll({
+                where:{
+                    id_user: req.id_user
+                },
+                include:[{
+                    model: User
+                }]
+            });
+        }
+        res.status(200).json(response);
+   } catch (error) {
+        res.status(500).json({msg: error.message});
+   }
 }
 
 export const getBidLogsById = (req, res) => {
--- backend/routes/BidLogsRoute.js
+++ backend/routes/BidLogsRoute.js
@@ -6,13 +6,14 @@ import {
     updateBidLogs,
     deleteBidLogs
 } from "../controllers/BidLogs.js";
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/tbid_logs', getBidLogs);
-router.get('/tbid_logs/:id', getBidLogsById);
-router.post('/tbid_logs', createBidLogs);
-router.patch('/tbid_logs/:id', updateBidLogs);
-router.delete('/tbid_logs/:id', deleteBidLogs);
+router.get('/tbid_logs',verifyUser, getBidLogs);
+router.get('/tbid_logs/:id',verifyUser, getBidLogsById);
+router.post('/tbid_logs',verifyUser, createBidLogs);
+router.patch('/tbid_logs/:id',verifyUser, updateBidLogs);
+router.delete('/tbid_logs/:id',verifyUser, deleteBidLogs);
 
 export default router;
