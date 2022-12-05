17cb088bce920f7ce9d455cb4e0ceb345e61047b
Yoga Anggara <69418956+Angga-LWA@users.noreply.github.com>
2022-12-05 15:47:34 +0700

    update product.js
    getProductById

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -3,18 +3,22 @@ import Branch from "../models/BranchModel.js";
 import Category from "../models/CategoryModel.js";
 import Entity from "../models/EntityModel.js";
 import Region from "../models/RegionModel.js";
+import {Op} from "sequelize";
 
 export const getProducts = async (req, res) =>{
    try {
         let response;
         if(req.role === "admin"){
             response = await Product.findAll({
+                attributes:['prdcode','prdname','prddesc'],
                 include:[{
-                    model: Branch, Category, Entity, Region
+                    model: Branch, Category, Entity, Region,
+                    attributes:['branchcode','branchname']
                 }]
             });
         }else{
             response = await Product.findAll({
+                attributes:['prdcode','prdname','prddesc'],
                 where:{
                     id_branch: req.id_branch,
                     id_category: req.id_category,
@@ -22,7 +26,8 @@ export const getProducts = async (req, res) =>{
                     id_region: req.id_region
                 },
                 include:[{
-                    model: Branch, Category, Entity, Region
+                    model: Branch, Category, Entity, Region,
+                    attributes:['branchcode','branchname']
                 }]
             });
         }
@@ -32,8 +37,42 @@ export const getProducts = async (req, res) =>{
    }
 }
 
-export const getProductById = (req, res) => {
-   
+export const getProductById = async (req, res) => {
+    try {
+        const product = await Product.findOne({
+            where:{
+                id: req.params.id
+            }
+        });
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"});
+        let response;
+        if(req.role === "admin"){
+            response = await Product.findOne({
+                attributes:['prdcode','prdname','prddesc'],
+                where:{
+                    id: product.id
+                },
+                include:[{
+                    model: Branch, Category, Entity, Region,
+                    attributes:['branchcode','branchname']
+                }]
+            });
+        }else{
+            response = await Product.findOne({
+                attributes:['prdcode','prdname','prddesc'],
+                where:{
+                    [Op.and]:[{id: product.id}, {id_branch: req.id_branch, id_category: req.id_category, id_entity: req.id_entity, id_region: req.id_region}]
+                },
+                include:[{
+                    model: Branch, Category, Entity, Region,
+                    attributes:['branchcode','branchname']
+                }]
+            });
+        }
+        res.status(200).json(response);
+   } catch (error) {
+        res.status(500).json({msg: error.message});
+   }
 }
 
 export const createProduct = async (req, res) => {
@@ -59,8 +98,33 @@ export const createProduct = async (req, res) => {
     }
 }
 
-export const updateProduct = (req, res) => {
-
+export const updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findOne({
+            where:{
+                id: req.params.id
+            }
+        });
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"});
+        const {prdcode, prdname, prddesc, price, min_multiples, location, auction_date, created_by} = req.body;
+        if(req.role === "admin"){
+            await Product.update({prdcode, prdname, prddesc, price, min_multiples, location, auction_date, created_by},{
+                where: {
+                    id: product.id
+                }
+            });
+        }else{
+            
+            await Product.update({prdcode, prdname, prddesc, price, min_multiples, location, auction_date, created_by},{
+                where:{
+                    [Op.and]:[{id: product.id}, {id_branch: req.id_branch, id_category: req.id_category, id_entity: req.id_entity, id_region: req.id_region}]
+                }
+            });
+        }
+        res.status(200).json(response);
+   } catch (error) {
+        res.status(500).json({msg: error.message});
+   }
 }
 
 export const deleteProduct = (req, res) => {
