c55a9be0a236de9fbab2c88826f379c18dbf538f
Yoga Anggara <69418956+Angga-LWA@users.noreply.github.com>
2022-12-07 16:24:50 +0700

    update blacklist
    update blacklist

--- backend/controllers/Blacklist.js
+++ backend/controllers/Blacklist.js
@@ -1,22 +1,27 @@
 import Blacklist from "../models/BlacklistModel.js";
 import User from "../models/UserModel.js";
+import { Op } from "sequelize";
 
 export const getBlacklist = async (req, res) => {
    try {
         let response;
         if(req.role === "admin"){
             response = await Blacklist.findAll({
+                attributes:['status','release_date','created_by'],
                 include:[{
-                    model: User
+                    model: User,
+                    attributes:['username','first_name','last_name','phone','birthday','birthplace','address_id','address_domisili','profile_img','email','password','flag_active','nik','npwp']
                 }]
             });
         }else{
             response = await Blacklist.findAll({
+                attributes:['status','release_date','created_by'],
                 where:{
                     id_user: req.id_user
                 },
                 include:[{
-                    model: User
+                    model: User,
+                    attributes:['username','first_name','last_name','phone','birthday','birthplace','address_id','address_domisili','profile_img','email','password','flag_active','nik','npwp']
                 }]
             });
         }
@@ -26,8 +31,42 @@ export const getBlacklist = async (req, res) => {
    }
 }
 
-export const getBlacklistById = (req, res) => {
-   
+export const getBlacklistById = async (req, res) => {
+    try {
+        const blacklist = await Blacklist.findOne({
+            where:{
+                id: req.params.id
+            }
+        })
+        if(!blacklist) return res.status(404).json({msg: "Data tidak ditemukan"});
+        let response;
+        if(req.role === "admin"){
+            response = await Blacklist.findAll({
+                attributes:['status','release_date','created_by'],
+                where:{
+                    id: blacklist.id
+                },
+                include:[{
+                    model: User,
+                    attributes:['username','first_name','last_name','phone','birthday','birthplace','address_id','address_domisili','profile_img','email','password','flag_active','nik','npwp']
+                }]
+            });
+        }else{
+            response = await Blacklist.findAll({
+                attributes:['status','release_date','created_by'],
+                where:{
+                    [Op.and]:[{id: blacklist.id}, {id_user: req.id_user}]
+                },
+                include:[{
+                    model: User,
+                    attributes:['username','first_name','last_name','phone','birthday','birthplace','address_id','address_domisili','profile_img','email','password','flag_active','nik','npwp']
+                }]
+            });
+        }
+        res.status(200).json(response);
+   } catch (error) {
+        res.status(500).json({msg: error.message});
+   }
 }
 
 export const createBlacklist = (req, res) => {
